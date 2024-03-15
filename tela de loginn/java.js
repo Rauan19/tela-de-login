6a1eaeec0ccf325e5fb8419a4511fef95809ba1f
Rauan19 <144159197+Rauan19@users.noreply.github.com>
2024-03-15 00:49:38 -0300

    Add files via upload

---
+++ tela de loginn/java.js
@@ -0,0 +1,7 @@
+const botao = document.querySelector('.botao')
+const cadastro = document.querySelector('.cadastro')
+
+botao.addEventListener('click', function conclusao(){
+    cadastro.style.display = 'block'
+}
+ )
